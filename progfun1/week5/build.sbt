66fbdb585ead54bc475fee96d4d7c4777cf228bb
Derek Fenson <derekfenson@gmail.com>
2017-11-05 22:36:41 -0800

    Adding code for week5

---
+++ progfun1/week5/build.sbt
@@ -0,0 +1,6 @@
+name := "week5"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
+        
---
+++ progfun1/week5/src/main/scala/myList.scala
@@ -0,0 +1,33 @@
+package myList
+
+object myList {
+
+  def init[T](xs: List[T]): List[T] = xs match {
+    case List() => throw new Error("init of empty list")
+    case List(_) => List()
+    case y :: ys => y :: init(ys)
+  }
+
+  def last[T](xs: List[T]): T = xs match {
+    case List() => throw new Error("last of empty list")
+    case List(x) => x
+    case y :: ys => last(ys)
+  }
+
+  def concat[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List() => ys
+    case z :: zs => z :: concat(zs, ys)
+  }
+
+  def reverse[T](xs: List[T]): List[T] = xs match {
+    case List() => List()
+    case y :: ys => reverse(ys) ++ List(y)
+  }
+
+  def removeAt(n: Int, xs: List[Int]) =
+    (xs take n) ++ (xs drop n+1)
+
+  def flatten(xs: List[Any]): List[Any] = ???
+
+
+}
