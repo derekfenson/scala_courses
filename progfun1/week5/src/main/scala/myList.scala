e01160ff84a5ef75dcb4481e69259a2703142fbb
Derek Fenson <derekfenson@gmail.com>
2017-11-06 22:22:04 -0800

    Finished week5

--- progfun1/week5/src/main/scala/myList.scala
+++ progfun1/week5/src/main/scala/myList.scala
@@ -2,6 +2,8 @@ package myList
 
 import math.Ordering
 
+
+
 object myList {
 
   def init[T](xs: List[T]): List[T] = xs match {
@@ -58,31 +60,56 @@ object myList {
   */
 
   def msort[T](xs: List[T])(implicit ord: Ordering[T]): List[T] = {
-    val n = xs.length/2
-    if (n==0) xs
+    val n = xs.length / 2
+    if (n == 0) xs
     else {
-      def merge[T](xs: List[T], ys: List[T]): List[T] =
-      (xs, ys) match {
-        case (_, Nil) => xs
-        case (Nil, _) => ys
-        case (x :: xs1, y :: ys1) =>
-          if (ord.lt(x, y)) x :: merge(xs1, ys)
-          else y :: merge(xs, ys1)
-      }
+      def merge(xs: List[T], ys: List[T]): List[T] =
+        (xs, ys) match {
+          case (xs, Nil) => xs
+          case (Nil, ys) => ys
+          case (x :: xs1, y :: ys1) =>
+            if (ord.lt(x, y)) x :: merge(xs1, ys)
+            else y :: merge(xs, ys1)
+        }
+
       val (fst, snd) = xs splitAt n
       merge(msort(fst), msort(snd))
     }
   }
 
+  def squareList1(xs: List[Int]): List[Int] = xs match {
+    case Nil => Nil
+    case y :: ys => y*y :: squareList1(ys)
+  }
 
+  def squareList2(xs: List[Int]): List[Int] =
+    xs map (x => x*x)
 
+  def pack[T](xs: List[T]): List[List[T]] = xs match {
+    case Nil     => Nil
+    case y :: _  =>
+      val (first, rest) = xs span (_==y)
+      first :: pack(rest)
+  }
 
+  def encode[T](xs: List[T]): List[(T, Int)] =
+    pack(xs) map (x => (x.head, x.length))
 
+  def mapFun[T, U](xs: List[T], f: T => U): List[U] =
+    (xs foldRight List[U]())(f(_) :: _)
+
+  def lengthFun[T](xs: List[T]): Int =
+    (xs foldRight 0)((x, y) => y+1)
 }
 
 object Main extends App {
   import myList._
   val list1=List(2,1,7,9,-10,12)
   val list2=msort(list1)
+  val list3=List('a','a','a','b','b','c', 'a','a')
+  println(pack(list3))
+  println(encode(list3))
+  println(list3.length)
+  println(lengthFun(list3))
 }
 
